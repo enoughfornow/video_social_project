70e75c1fe90466a4a553d7164b601227751451d6
Dmitry <enoughfornowbusiness@gmail.com>
2022-09-19 23:49:18 +0300

    work with upload video add validation

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -12,7 +12,7 @@ import { createOrGetUser } from '../../utils';
 import useAuthStore from '../../store/authStore';
 
 export const Navbar = () => {
-  const { userProfile, addUser } = useAuthStore();
+  const { userProfile, addUser, removeUser } = useAuthStore();
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -30,6 +30,29 @@ export const Navbar = () => {
                 <span className="hidden md:block">Upload</span>
               </button>
             </Link>
+            {userProfile.image && (
+              <Link href="/">
+                <>
+                  <Image
+                    width={40}
+                    height={40}
+                    className="rounded-full cursor-pointer"
+                    src={userProfile.image}
+                    alt="profile logo"
+                  />
+                </>
+              </Link>
+            )}
+            <button type="button" className="px-2">
+              <AiOutlineLogout
+                color="red"
+                fontSize={21}
+                onClick={() => {
+                  googleLogout();
+                  removeUser();
+                }}
+              />
+            </button>
           </div>
         ) : (
           <GoogleLogin
---
+++ pages/upload.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import axios from 'axios';
+
+import { SanityAssetDocument } from '@sanity/client';
+
+import { FaCloudUploadAlt } from 'react-icons/fa';
+import { MdDelete } from 'react-icons/md';
+import { client } from '../utils/client';
+
+const Upload = () => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [videoAsset, setVideoAsset] = React.useState<SanityAssetDocument | undefined>();
+  const [wrongFileType, setWrongFileType] = React.useState(false);
+
+  const uploadVideo = async (e: any) => {
+    const selectedFile = e.target.files[0];
+    const fileTypes = ['video/mp4', 'video/webm', 'video/ogg'];
+    if (fileTypes.includes(selectedFile.type)) {
+      client.assets
+        .upload('file', selectedFile, {
+          contentType: selectedFile.type,
+          filename: selectedFile.name,
+        })
+        .then((data) => {
+          setVideoAsset(data);
+          setIsLoading(false);
+        });
+    } else {
+      setIsLoading(false);
+      setWrongFileType(true);
+    }
+  };
+  return (
+    <div className="flex w-full h-full">
+      <div className="bg-white rounded-lg">
+        <div>
+          <div>
+            <p className="text-2xl font-bold">Upload Video</p>
+            <p className="text-md text-gray-400 mt-1">Add video to your account</p>
+          </div>
+          <div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100">
+            {isLoading ? (
+              <p>Uploading...</p>
+            ) : (
+              <div>
+                {videoAsset ? (
+                  <div>
+                    <video
+                      src={videoAsset.url}
+                      loop
+                      controls
+                      className="rounded-xl h-[450px] mt-16 bg-black"></video>
+                  </div>
+                ) : (
+                  <label className="cursor-pointer">
+                    <div className="flex flex-col items-center justify-center h-full">
+                      <div className="flex flex-col items-center justify-center">
+                        <p className="font-bold text-xl">
+                          <FaCloudUploadAlt className="text-gray-300 text-6xl" />
+                        </p>
+                        <p className="font-semibold text-xl">Upload Video</p>
+                      </div>
+                      <p className="text-gray-400 text-center mt-10 text-sm leading-7">
+                        MP4 or WebM <br />
+                        720x1280 or higher <br />
+                        Up to 10 min <br />
+                        Less than 2GB
+                      </p>
+                      <p className="bg-[#213123] text-center mt-10 rounded text-white text-md font-medium p-2 w-52 outline-none">
+                        Select File
+                      </p>
+                    </div>
+                    <input
+                      type="file"
+                      name="upload-video"
+                      className="w-0 h-0"
+                      onChange={uploadVideo}
+                    />
+                  </label>
+                )}
+              </div>
+            )}
+            {wrongFileType && (
+              <p className="text-center text-xl text-red-400 font-semibold mt-4 w-[250px]">
+                Please select a video file
+              </p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Upload;
